7aa628ba15445161c882b94dbc708759dd2bdef7
Inga <inga@local.machine>
2023-02-08 12:58:41 +0200

    add cart page

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,94 @@
+// 1. cart atvaizduosim items (kaina x quantity)
+// 1.1 Button apmoketi numeta i login/register page
+// 2. Login page
+// 3. register page
+// 4.AuthContext susitvarkom, kad saugotu user
+
+import { useContext } from 'react';
+import { ProductContext } from '../../contexts/ProductContext';
+import styled from 'styled-components';
+import { euroSymbol } from '../../consts/currency';
+import { screenSize } from '../../consts/mediaQueries';
+import Button from '../../components/Button/Button';
+
+const Cart = () => {
+  const { products } = useContext(ProductContext);
+
+  const cartProducts = products.slice(0, 2);
+  console.log(cartProducts);
+
+  return (
+    <Container>
+      <Header>
+        <h2>MY BAG</h2>
+        <p>Item are reserved for 30 minutes</p>
+      </Header>
+      <CartContainer>
+        {cartProducts.map((product) => (
+          <CartItem key={product.id}>
+            <img src={product.picUrl[0]} alt={product.name} />
+            <div>
+              <CartItemPrice>
+                {euroSymbol}
+                {product.price}
+              </CartItemPrice>
+              <p>{product.name}</p>
+              <CartItemColor>{product.color}</CartItemColor>
+            </div>
+          </CartItem>
+        ))}
+      </CartContainer>
+      <ButtonContainer>
+        <Button>Checkout</Button>
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default Cart;
+
+const Container = styled.div`
+  max-width: ${screenSize.tablet};
+  margin: 0 auto;
+`;
+
+const Header = styled.div`
+  padding: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 4px;
+  margin-bottom: 24px;
+`;
+
+const CartContainer = styled.div`
+  background-color: #fff;
+  margin-bottom: 24px;
+`;
+
+const CartItem = styled.div`
+  display: flex;
+  img {
+    margin-right: 8px;
+    object-fit: contain;
+    width: 100px;
+  }
+`;
+
+const CartItemPrice = styled.p`
+  margin-top: 16px;
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 8px;
+`;
+
+const CartItemColor = styled.p`
+  margin-top: 8px;
+  font-weight: 300;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
