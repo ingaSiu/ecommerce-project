9b325697b63c5ad16f7d390d45bed948119146d3
ingaSiu <ingasiud@gmail.com>
2023-02-11 10:11:33 +0200

    new context, nested contexts

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,11 @@
+import { createContext, useState } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  console.log(user);
+  return <UserContext.Provider value={{ user, setUser }}> {children}</UserContext.Provider>;
+};
+
+export { UserContext, UserProvider };
